4283096a049b7f3a032398a8ebce88dc060bb25f
Joel Buikema <j.buikema@sportradar.com>
2023-04-25 17:10:47 -0400

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,26 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive class="d-flex align-center  fill-height">
+    <v-responsive class="d-flex align-center fill-height">
 
-      <h1 class="text-h3 font-weight-bold text-center">WORK HISTORY</h1>
+      <h3 class="text-h3 font-weight-bold text-center">WORK HISTORY</h3>
 
       <div class="py-14" />
 
-      <v-timeline side="end">
+      <v-row no-gutters class="d-flex align-center justify-sm-space-between">
+
+        <v-timeline side="end">
         <v-timeline-item
           v-for="(job, i) in history.jobs"
           :key="i"
+          size="small"
         >
           <template v-slot:opposite>
             <h3 class="font-weight-light">
               {{job.start_year}}-{{job.end_year}}
             </h3>
           </template>
-          <v-card>
+          <v-col cols="12">
+          <v-card class="mx-auto" elevation="0">
             <v-card-title class="primary text-center">
               <h2 class="font-weight-light">
                 {{job.title}}
@@ -26,21 +30,25 @@
               </h4>
             </v-card-title>
             <v-card-text>
-              <p>{{job.description}}</p>
+              <p class="font-weight-medium text-body-2">{{job.description}}</p>
               <br/>
-
-              <ul class="ps-5">
-                <li
-                v-for="(skill, i) in job.responsibilities"
-                v-text="skill"
-                :key="i"
-                />
-              </ul>
+              <v-list density="compact">
+                <v-list-item
+                  v-for="(item, i) in job.responsibilities"
+                  :key="i"
+                  :value="item"
+                >
+                <template v-slot:prepend>
+                  <v-icon icon="mdi-circle-medium"></v-icon>
+                </template>
+                  {{item}}
+                </v-list-item>
+              </v-list>
             </v-card-text>
           </v-card>
+          </v-col>
         </v-timeline-item>
       </v-timeline>
-      <v-row class="d-flex align-center justify-center">
       </v-row>
     </v-responsive>
   </v-container>
--- src/store/app.js
+++ src/store/app.js
@@ -13,9 +13,11 @@ export const useWorkHistory = defineStore('app', {
           "graphics applications that create and deliver customized solutions to major networks\n" +
           "in a dynamic live sports environment.",
         responsibilities: [
-          "Deploy and test unique broadcast graphics software solutions to enterprise customers (CBS, FOX, NBC, Bally Sports etc.) with hundreds of millions of TV viewers" +
+          "Deploy and test unique broadcast graphics software solutions to enterprise customers\n" +
+          "(CBS, FOX, NBC, Bally Sports etc.) with hundreds of millions of TV viewers",
           "Utilize Python (Pandas, Flask) and TypeScript programming languages",
-          "Employ data-driven architecture and systems design, including RESTful APIs microservices architecture, GraphQL, SQL, NoSQL, and In-memory stores",
+          "Employ data-driven architecture and systems design, including RESTful APIs,\n" +
+          "microservices architecture, GraphQL, SQL, NoSQL, and In-memory stores",
           "Implement AWS services such as S3, Lambda, Step functions, and API Gateway to build scalable and reliable serverless applications",
           "Develop and maintain CI/CD pipelines using AWS CodePipeline and CodeBuild",
           "Utilize AWS CloudFormation to define and deploy infrastructure as code"
@@ -35,27 +37,40 @@ export const useWorkHistory = defineStore('app', {
           "Design and Develop internal applications in Python, Javascript, SQL & PostgreSQL",
           "Oversee and maintain NAS, SAN, Windows & Linux servers, network & virtual environment"
         ]
-      }
-      // {
-      //   title: "",
-      //   company: "",
-      //   start_year: "",
-      //   end_year: "",
-      //   description: null,
-      //   responsibilities: [
-      //     ""
-      //   ]
-      // },
-      // {
-      //   title: "",
-      //   company: "",
-      //   start_year: "",
-      //   end_year: "",
-      //   description: null,
-      //   responsibilities: [
-      //     ""
-      //   ]
-      // }
+      },
+      {
+        title: "AV Project Manager & Systems Engineer",
+        company: "Technical Operations Inc.",
+        start_year: "2012",
+        end_year: "2013",
+        description: null,
+        responsibilities: [
+          "Design and oversee AV installations for corporations and schools",
+          "Service calls and consulting on IT systems"
+        ]
+      },
+      {
+        title: "Lead Audio Visual Technician",
+        company: "Presentation Products Inc. & Shadowbox Design Mgmt. & PSA",
+        start_year: "2009",
+        end_year: "2012",
+        description: null,
+        responsibilities: [
+          "Lead AV technician & audio specialist (A1) for corporate and commercial events, conferences, presentations and meetings, primarily at Scholastic Inc.",
+          "Install SMART boards, Crestron systems, projectors, digital signage, configuring DSPs, Clearone and Cisco teleconferencing",
+          "Work with PA systems, mics, antenna systems, projectors, screens, video distribution & switcher systems and custom cable creation Polycom & Tandberg teleconferencing & TriCaster systems"
+        ]
+      },
+      {
+        title: "Chief Audio Engineer & Mixer ",
+        company: "Recording studios and live venues",
+        start_year: "2005",
+        end_year: "2009",
+        description: null,
+        responsibilities: [
+          "Joes Public Theater, Kumble Theater (LIU), The Hook, Rocks Off etc..."
+        ]
+      },
 
     ]
   }),
